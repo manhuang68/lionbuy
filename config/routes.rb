5b9a4ae033bed2c6ef724179c41386c4e10b5807
manhuang68 <manhuang1996@gmail.com>
2022-10-29 05:39:01 -0400

    login and register front end

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rottenpotatoes::Application.routes.draw do
   resources :movies
   # map '/' to be a redirect to '/movies'
   root :to => redirect('/movies')
-  get 'similar_movies/:title' => 'movies#search', as: :search_similar_movies
+  get 'similar_movies/:title' => 'movies#search', as: :search_similar_movie
+  get 'signup/' => 'movies#signup', as: :signup
+  get 'signin/' => 'movies#signin', as: :signin
 end
