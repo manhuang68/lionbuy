b01cc348b4c7ec23adfec6a3e92fc10a37f7c8e0
Kent Wu <kentwhf@gmail.com>
2022-10-31 14:14:51 -0400

    Feature search and filter

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,45 +1,51 @@
 class PostsController < ApplicationController
 
   def show
+    # Prevent hacker
     if session[:user_id] == nil
       redirect_to "/signin"
     end
     id = params[:id] # retrieve post ID from URI route
     @post = Post.find(id) # look up post by unique ID
-    # will render app/views/posts/show.<extension> by default
   end
 
   def index
+    # Prevent hacker
+    if session[:user_id] == nil
+      redirect_to "/signin"
+    end
+
     @posts = Post.all
-    # if not params[:categories] and not params[:sort]
-    #   if session[:sort] or session[:categories]
-    #     params[:categories] = session[:categories]
-    #     params[:sort] = session[:sort]
-    #     redirect_to posts_path({:categories => params[:categories], :sort => params[:sort]})
-    #     return
-    #   end
-    # end
-    # session.delete(:categories)
-    # session.delete(:sort)
+    if not params[:categories] and not params[:keyword]
+      if session[:categories] or session[:keyword]
+        params[:categories] = session[:categories]
+        params[:keyword] = session[:keyword]
+        redirect_to posts_path({:categories => params[:categories], :keyword => params[:keyword]})
+        return
+      end
+    end
+    session.delete(:categories)
+    session.delete(:keyword)
 
     @all_categories = Post.all_categories
     @categories_to_show = params[:categories] ? params[:categories].keys : @all_categories
-    # @title_header_hilite_to_show = params[:sort] == 'title' ? 'hilite bg-warning' : ''
-    # @release_date_header_hilite_to_show = params[:sort] == 'release_date' ? 'hilite bg-warning' : ''
-    
-    # @posts = Post.with_categories(@categories_to_show)
-    # if params[:sort]
-    #   @posts = @posts.order(params[:sort])
-    #   session[:sort] = params[:sort]
-    # end
+    @posts = Post.with_categories(@categories_to_show)
+    @keyword_to_show = params[:keyword] ? params[:keyword] : ""
 
-    # if params[:categories]
-    #   session[:categories] = params[:categories]
+    res = []
+    @posts.each do |post| 
+      if post.with_keyword(@keyword_to_show)
+          res.append(post)
+      end
+    end
+    @posts = res
 
-    # @movies = Movie.all
     # byebug
-    if session[:user_id] == nil
-      redirect_to "/signin"
+    if params[:categories]
+      session[:categories] = params[:categories]
+    end
+    if params[:keyword]
+      session[:keyword] = params[:keyword]
     end
   end
 
@@ -57,16 +63,28 @@ class PostsController < ApplicationController
   end
 
   def create
+    # Prevent hacker
+    if session[:user_id] == nil
+      redirect_to "/signin"
+    end
     @post = Post.create!(post_params)
     flash[:notice] = "#{@post.item} was successfully created."
     redirect_to posts_path
   end
 
   def edit
+      # Prevent hacker
+      if session[:user_id] == nil
+        redirect_to "/signin"
+      end
     @post = Post.find params[:id]
   end
 
   def update
+    # Prevent hacker
+    if session[:user_id] == nil
+      redirect_to "/signin"
+    end
     @post = Post.find params[:id]
     @post.update_attributes!(post_params)
     flash[:notice] = "#{@post.item} was successfully updated."
@@ -74,6 +92,10 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    # Prevent hacker
+    if session[:user_id] == nil
+      redirect_to "/signin"
+    end
     @post = Post.find(params[:id])
     @post.destroy
     flash[:notice] = "Post '#{@post.item}' deleted."
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,24 @@
 class Post < ActiveRecord::Base
+    def with_keyword(with_keyword)
+        if with_keyword.length != 0
+            if (self.description.downcase.include? with_keyword)
+                return true
+            elsif (self.item.downcase.include? with_keyword)
+                return true
+            end
+            return false
+        end
+        return true
+    end
+
     def self.with_categories(categories_list)
         if categories_list.length != 0
-            return Post.all 
+            return Post.where(category: categories_list) 
         else
             return Post.all 
         end
     end
-    
+
     def self.all_categories()
         @all_categories = ['Electronics','Bedding','Education']
     end 
