2663c31b111935b00d05ac0a5423a590f5d028af
manhuang68 <manhuang1996@gmail.com>
2022-12-11 17:05:39 -0500

    update orders and bids spec controller testing

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -13,28 +13,39 @@ RSpec.describe OrdersController, type: :controller do
       end
       @post = nil
       if Post.where(:item => "IBM laptop").empty?
-      @post = Post.create(:item => 'IBM laptop', :description => 'Metal Platform Bed Frame with Headboard', :price => '120', :user => 'SamAlexander', :email => 'sa6156@columbia.edu', :category => 'Bedding', :buy_now => true, :bid => true, :start_bid => '100', :current_bid => '105', :closed => false)
+        @post = Post.create(:item => 'IBM laptop', :description => 'Metal Platform Bed Frame with Headboard', :price => '120', :user => 'SamAlexander', :email => 'sa6156@columbia.edu', :category => 'Bedding', :buy_now => true, :bid => true, :start_bid => '100', :current_bid => '105', :closed => false)
+        Bid.create(:product_id => @post.id,  :user_id => "1", :bid => "105")
       end
+
       @post2 = nil
       if Post.where(:item => "Headphones").empty?
       @post2 =  Post.create(:item => 'Headphones', :description => 'Textbooks for freshman to senior year', :price => '10', :user => 'MikeMckenzie', :email => 'mm4111@columbia.edu', :category => 'Education', :buy_now => false, :bid => true, :start_bid => '5', :current_bid => '5', :closed => false)
       end
   end
 
-
-
   describe "Buy and Item" do
-    it "Buying an IBM laptop and return successfully" do
+    it "Buying my own item and returns invalid" do
       session[:user_id] = "1"
-      get :create, {:order => {:product_id => @post.id}}
-      expect(response).to redirect_to order_history_path
-      expect(flash[:notice]).to match('Your order for IBM laptop has been placed!')
+      session[:email] = "mm4111@columbia.edu"
+      get :create, {:order => {:product_id => @post2.id}}
+      expect(response).to redirect_to redirect_to post_path(@post2)
+      expect(flash[:warning]).to match('You are not allowed to buy your own item')
     end
     it "Buying an IBM laptop and return successfully" do
       session[:user_id] = "1"
+      session[:email] = nil
       get :create, {:order => {:product_id => @post2.id}}
       expect(response).to redirect_to order_history_path
       expect(flash[:notice]).to match('Your order for Headphones has been placed!')
     end
-end
+  end
+
+  describe "Accept a deal for a bid" do
+    it "Accept the deal for an IBM laptop and return successfully" do
+      session[:user_id] = "1"
+      get :accept_bid, {:order => {:product_id => @post.id}}
+      expect(response).to redirect_to selling_history_path
+      expect(flash[:notice]).to match('You accepted the deal for IBM laptop !')
+    end
+  end
 end
